5b78478c813273fb889c09cfcdffd66f8ff54bc5
John N <43926285+jcniedfeldt@users.noreply.github.com>
2019-01-09 13:13:54 -0700

    No errors. Finished stop 8

--- liri.js
+++ liri.js
@@ -1,2 +1,12 @@
 // LIRI
 // Purpose is to to take in parameters and search multiple databases for relevant results
+
+// npm installations
+
+// dotenv will be used to set process.env variables, including spotify_id and secret
+
+require("dotenv").config();
+var keys = require("./keys");
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
